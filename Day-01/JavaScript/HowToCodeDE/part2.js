0e1e5ad0032d9aa821cf9629ea7a627a32727e94
Florian Reintgen <85225228+HowToCodeDE@users.noreply.github.com>
2023-12-01 14:44:53 +0100

    Create part2.js

---
+++ Day-01/JavaScript/HowToCodeDE/part2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let data = fs.readFileSync("input.txt").toString();
+const wordToDigit = { one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9' };
+let values = data.split(/\r?\n|\r|\n/g);
+let addedNums = 0;
+values.forEach(value => {
+    value = value.replace(/(one|two|three|four|five|six|seven|eight|nine)/g, match => match + match[match.length - 1]);
+    value = value.replace(/(one|two|three|four|five|six|seven|eight|nine)/g, match => wordToDigit[match] || match);
+    value = value.replace(/[a-z]+/g, '');
+    value = addedNums + parseInt(value[0] + value[value.length - 1]);
+});
+console.log(addedNums);
