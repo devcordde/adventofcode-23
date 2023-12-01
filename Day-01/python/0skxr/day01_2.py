29deaac07b9c5ce0802717d0c18b4b1abc21562a
Oskar Schulz <vollkorncrafter@gmail.com>
2023-12-01 12:25:09 +0100

    made day 1:02 a line shorter

--- Day-01/python/0skxr/day01_2.py
+++ Day-01/python/0skxr/day01_2.py
@@ -1,4 +1,3 @@
-import string
 numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"]
 value = 0
 f = open("day1.txt", "r")
@@ -7,6 +6,6 @@ for line in f.readlines():
   for i in range(len(line)):
     for n in numbers:
       if line[i:i + 5].startswith(n): current.append(str(numbers.index(n)))
-    if line[i] in string.digits: current.append(line[i])
+    if line[i] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]: current.append(line[i])
   value += int(current[0] + current[-1])
 print(value)
