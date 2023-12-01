513256c8ff30cc26631460fedf4817ad2a4c9de2
Florian Reintgen <85225228+HowToCodeDE@users.noreply.github.com>
2023-12-01 17:24:08 +0100

    fixed a mistake and simplified some things

--- Day-01/javascript/HowToCodeDE/part2.js
+++ Day-01/javascript/HowToCodeDE/part2.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
-let data = fs.readFileSync("input.txt").toString();
+const data = fs.readFileSync("input.txt").toString();
 const wordToDigit = { one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9' };
-let values = data.split(/\r?\n|\r|\n/g);
+const values = data.split(/\r?\n|\r|\n/g);
 let addedNums = 0;
+const exp = /(one|two|three|four|five|six|seven|eight|nine)/g;
 values.forEach(value => {
-    value = value.replace(/(one|two|three|four|five|six|seven|eight|nine)/g, match => match + match[match.length - 1]);
-    value = value.replace(/(one|two|three|four|five|six|seven|eight|nine)/g, match => wordToDigit[match] || match);
+    value = value.replace(exp, match => match + match[match.length - 1]);
+    value = value.replace(exp, match => wordToDigit[match] || match); // Remove this line for part 1
     value = value.replace(/[a-z]+/g, '');
-    value = addedNums + parseInt(value[0] + value[value.length - 1]);
+    addedNums += parseInt(value[0] + value[value.length - 1]);
 });
 console.log(addedNums);
