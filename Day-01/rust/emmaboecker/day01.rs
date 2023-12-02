e4baa5386ee8cf0a263087faaa971c89612172a9
Emma Böcker <accs.stckoverflw@gmail.com>
2023-12-02 10:49:19 +0100

    Add Day 2 Solution in Rust and improve my solution from yesterday (#25)
    * add day 2 solution in rust
    
    * improve my solution from yesterday
    
    * run rustfmt
    
    * remove unnecessary collect in day 1

--- Day-01/rust/emmaboecker/day01.rs
+++ Day-01/rust/emmaboecker/day01.rs
@@ -1,56 +1,40 @@
-use std::collections::HashMap;
+const WRITTEN_NUMBERS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
-fn main() {
+pub fn main() {
     let input = include_str!("../../input/day01.txt");
-
     println!("Part 1: {}", find_numbers(input, false));
-
     println!("Part 2: {}", find_numbers(input, true));
 }
 
 fn find_numbers(input: &str, written: bool) -> u32 {
-    let output = input
+    input
         .lines()
         .map(|line| {
             let valid = find_valid(line, written);
             format!("{}{}", valid.first().unwrap(), valid.last().unwrap())
+                .parse::<u32>()
+                .unwrap()
         })
-        .collect::<Vec<String>>();
-
-    output
-        .iter()
-        .map(|string| string.parse::<u32>().unwrap())
-        .sum::<u32>()
+        .sum()
 }
 
 fn find_valid(input: &str, written: bool) -> Vec<u32> {
     let mut valid = vec![];
 
     if written {
-        let written_numbers = HashMap::from([
-            ("one", 1),
-            ("two", 2),
-            ("three", 3),
-            ("four", 4),
-            ("five", 5),
-            ("six", 6),
-            ("seven", 7),
-            ("eight", 8),
-            ("nine", 9),
-        ]);
-
-        for (written, value) in written_numbers {
+        for (index, written) in WRITTEN_NUMBERS.iter().enumerate() {
             let indices = input.match_indices(written);
 
-            for (index, _) in indices {
-                valid.push((index, value));
+            for (position, _) in indices {
+                valid.push((position, index + 1));
             }
         }
     }
 
     for value in 1..10 {
-        let string_value = value.to_string();
-        let indices = input.match_indices(&string_value);
+        let indices = input.match_indices((value as u8 + b'0') as char);
 
         for (index, _) in indices {
             valid.push((index, value));
@@ -60,6 +44,6 @@ fn find_valid(input: &str, written: bool) -> Vec<u32> {
     valid.sort_by(|a, b| a.0.cmp(&b.0));
     valid
         .into_iter()
-        .map(|(_, value)| value)
+        .map(|(_, value)| value as u32)
         .collect::<Vec<u32>>()
 }
