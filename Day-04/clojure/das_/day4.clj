50d4427e26c156099b04bf1cf790ec1661ef72fa
Daniel Scherf <munita.gavai@gmail.com>
2023-12-04 19:22:21 +0100

    Slightly improve Clojure Day04 again

--- Day-04/clojure/das_/day4.clj
+++ Day-04/clojure/das_/day4.clj
@@ -1,4 +1,4 @@
-(ns aoc-2023.day4
+(ns day4
     (:require [clojure.string :as str]))
 
 (defn input-parts [line]
@@ -19,15 +19,15 @@
 (defn extract-card-parts [line]
       (let [parts (input-parts line)
             numbers (last parts)]
-               {:game-number (game-number (first parts))}
-                :card-parts (->> (str/split numbers #" \| ")
-                                        (map string-to-ints))))
+           {:game-number (game-number (first parts))
+            :card-parts (->> (str/split numbers #" \| ")
+                             (map string-to-ints))}))
 
 (defn intersection [s]
       (filter (set (first s)) (last s)))
 
-(defn associate-count [y count]
-      (hash-map y count))
+(defn associate-count [element count]
+      (hash-map element count))
 
 (defn next-numbers [start, n, count]
       (->> (range start (+ start n))
@@ -38,7 +38,7 @@
       (let [matches (count (:card-parts card2))
             card-number (:game-number card2)
             new-cards (next-numbers (+ 1 card-number) matches (+ 1 (get card1 card-number 0)))]
-               (merge-with + (dissoc card1 :game-number) (dissoc card2 :card-parts) new-cards)))
+           (merge-with + (dissoc card1 :game-number) (dissoc card2 :card-parts) new-cards)))
 
 (defn part1 [lines]
       (->> lines
@@ -63,10 +63,10 @@
                         (map update-first-element)
                         (reduce calculate-score))
                    :game-number :card-parts)
-               (map val)
-               (reduce +)))
+           (map val)
+           (reduce +)))
 
 (defn -main []
-      (let [lines (clojure.string/split-lines (slurp "input.txt"))]
-               (println "Part 1: " (part1 lines))
-               (println "Part 2: " (part2 lines))))
+      (let [lines (str/split-lines (slurp "input.txt"))]
+           (println "Part 1: " (part1 lines))
+           (println "Part 2: " (part2 lines))))
