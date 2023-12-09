397c55221139be9474f4e1b7cecf58ae353603a4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-12-09 16:14:01 +0100

    further work on clojure day 8

--- Day-08/clojure/JohnnyJayJay/day8.clj
+++ Day-08/clojure/JohnnyJayJay/day8.clj
@@ -2,11 +2,13 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [[directions _ & nodes]]
-  {:directions (map (partial str/index-of "LR") directions)
-   :nodes (->> nodes
-               (map (partial re-seq #"[A-Z0-9]+"))
-               (map (juxt first (comp vec rest)))
-               (into {}))})
+  (let [dirs (map (partial str/index-of "LR") directions)]
+    {:directions dirs
+     :length (count dirs)
+     :nodes (->> nodes
+                 (map (partial re-seq #"[A-Z0-9]+"))
+                 (map (juxt first (comp vec rest)))
+                 (into {}))}))
 
 (def real-input (parse-input (str/split-lines (slurp "input"))))
 
@@ -38,14 +40,29 @@ XXX = (XXX, XXX)")))
 (defn find-cycle [{:keys [directions nodes]} start]
   (loop [current start
          steps 0
-         visited {}
-         [next-direction & rem] (cycle directions)]
-    (if-let [cycle-start-steps (get visited current)]
-      [cycle-start-steps (- steps cycle-start-steps)]
-      (recur (get-in nodes [current next-direction])
-             (inc steps)
-             (assoc visited current steps)
-             rem))))
+         visited {start 0}]
+    (let [result (reduce (comp (partial get-in nodes) vector) current directions)]
+      (if-let [cycle-start-steps (get visited result)]
+        {:start cycle-start-steps
+         :length (inc (- steps cycle-start-steps))
+         :end-states (select-keys visited (filter #(str/ends-with? % "Z") (keys visited)))}
+        (recur
+         result
+         (inc steps)
+         (assoc visited result (inc steps)))))))
+
+(defn part-1 [{:keys [length] :as input}]
+  (-> (find-cycle input "AAA")
+      (get-in [:end-states "ZZZ"])
+      (* length)))
+
+;; TODO: use gcd, more general solution
+(defn part-2 [{:keys [length nodes] :as input}]
+  (let [cycles (map (partial find-cycle input) (filter #(str/ends-with? % "A") (keys nodes)))]
+    (->> cycles
+         (map :length)
+         (reduce *)
+         (* length))))
 
 (defn steps [{:keys [directions nodes]}]
   (loop [current "AAA"
