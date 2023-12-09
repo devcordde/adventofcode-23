4adf83037936a7628a148d981ef5850b16446040
Daniel Scherf <munita.gavai@gmail.com>
2023-12-09 13:25:06 +0100

    It can't possibly get any shorter than this

--- Day-09/python/das_/day9.py
+++ Day-09/python/das_/day9.py
@@ -4,12 +4,9 @@ def find_step(sequence, part2):
 
     merged_sequence = [sequence[i] - sequence[i - 1] for i in range(1, len(sequence))]
 
-    if part2:
-        return sequence[0] - find_step(merged_sequence, True)
-    else:
-        return sequence[-1] + find_step(merged_sequence, False)
+    return sequence[0 if part2 else -1] + (-1 if part2 else 1) * find_step(merged_sequence, part2)
 
 
-sequences = [list(map(lambda x: int(x), line.split())) for line in open("input9.txt").read().split("\n")]
+sequences = [list(map(lambda x: int(x), line.split())) for line in open("input.txt").read().split("\n")]
 print(f"Part 1: {sum(map(lambda sequence: find_step(sequence, False), sequences))}")
 print(f"Part 2: {sum(map(lambda sequence: find_step(sequence, True), sequences))}")
