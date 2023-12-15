50cc9adaa20d54de4769fc3dd18387d43edb3bb7
Daniel Scherf <munita.gavai@gmail.com>
2023-12-15 18:38:09 +0100

    Add Python Day15

---
+++ Day-15/python/das_/day15.py
@@ -0,0 +1,33 @@
+from functools import reduce
+
+
+def calculate_box(string):
+    return reduce(lambda value, char: (value + ord(char)) * 17 % 256, string, 0)
+
+
+sequences = [sequence for sequence in open("input15.txt").read().replace("\n", "").split(",")]
+boxes = {}
+for i in range(256): boxes.setdefault(i, [])
+
+for sequence in sequences:
+    if sequence.count("-") == 1:
+        label = sequence[:-1]
+        box = calculate_box(label)
+
+        for element in boxes[box]:
+            if element[0] == label:
+                boxes[box].remove(element)
+    else:
+        (label, focal_length) = sequence.split("=")
+        box = calculate_box(label)
+        found = False
+        for i, element in enumerate(boxes[box]):
+            if element[0] == label:
+                boxes[box][i] = (element[0], focal_length)
+                found = True
+
+        if not found:
+            boxes[box].append((label, focal_length))
+
+print(f"Part 1: {sum([calculate_box(sequence) for sequence in sequences])}")
+print(f"Part 2: {sum(map(sum, map(lambda box: map(lambda item: (box[0] + 1) * (item[0] + 1) * int(item[1][1]), enumerate(box[1])), boxes.items())))}")
