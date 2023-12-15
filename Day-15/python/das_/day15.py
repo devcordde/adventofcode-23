3fb587f2d44f856399894686402388decd2ebdb6
Daniel Scherf <munita.gavai@gmail.com>
2023-12-16 00:03:34 +0100

    Cleanup Day15 solution

--- Day-15/python/das_/day15.py
+++ Day-15/python/das_/day15.py
@@ -5,29 +5,30 @@ def hash(string):
     return reduce(lambda value, char: (value + ord(char)) * 17 % 256, string, 0)
 
 
-sequences = [sequence for sequence in open("input15.txt").read().replace("\n", "").split(",")]
+sequences = open("input15.txt").read().split(",")
 boxes = {}
-for i in range(256): boxes.setdefault(i, [])
+for i in range(256):
+    boxes.setdefault(i, [])
 
 for sequence in sequences:
     if "-" in sequence:
         label = sequence[:-1]
-        box = hash(label)
+        box_index = hash(label)
 
-        for element in boxes[box]:
+        for element in boxes[box_index]:
             if element[0] == label:
-                boxes[box].remove(element)
+                boxes[box_index].remove(element)
     else:
         (label, focal_length) = sequence.split("=")
-        box = hash(label)
+        box_index = hash(label)
         found = False
-        for i, element in enumerate(boxes[box]):
+        for i, element in enumerate(boxes[box_index]):
             if element[0] == label:
-                boxes[box][i] = (element[0], focal_length)
+                boxes[box_index][i] = (element[0], focal_length)
                 found = True
 
         if not found:
-            boxes[box].append((label, focal_length))
+            boxes[box_index].append((label, focal_length))
 
-print(f"Part 1: {sum([hash(sequence) for sequence in sequences])}")
+print(f"Part 1: {sum(map(hash, sequences))}")
 print(f"Part 2: {sum(map(sum, map(lambda box: map(lambda item: (box[0] + 1) * (item[0] + 1) * int(item[1][1]), enumerate(box[1])), boxes.items())))}")
